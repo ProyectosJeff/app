ca1a11a5c9ee27b915881dac45e37f03a97e2152
Jeff <jeff574@hotmail.com>
2025-09-01 06:06:39 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./styles/index.css"; // tu hoja global (la de fondo azul)
+import "./index.css"; // tu hoja global (la de fondo azul)
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,3 +11,4 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
