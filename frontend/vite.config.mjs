bffa47cef7cb71ee78af58ead1691ee3f5588389
Jeff <jeff574@hotmail.com>
2025-09-01 04:48:40 -0500

    Update vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,11 +1,12 @@
-// frontend/vite.config.js
+// frontend/vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {} // evita problemas si alguna lib intenta leer process.env en el navegador
+    // evita crashes si alguna lib intenta leer process.env en el navegador
+    'process.env': {}
   },
   build: { target: 'es2018' }
 })
