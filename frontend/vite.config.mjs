ca9d1d46bbe287ed092eda42dc1b987a13d6e4ee
Jeff <jeff574@hotmail.com>
2025-09-01 05:13:08 -0500

    Update vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,17 +1,8 @@
-// frontend/vite.config.mjs
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Algunos paquetes referencian process o global; con esto evitamos errores.
-    "process.env": {},
-    global: "window",
-  },
-  build: {
-    target: "es2018",
-    // si necesitas depurar en producción, habilita temporalmente source maps:
-    // sourcemap: true,
-  },
+  server: { host: true, port: 5173 },
+  build: { outDir: "dist" } // Netlify public = dist
 });
