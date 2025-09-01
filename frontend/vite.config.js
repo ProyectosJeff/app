103fe41f61a1c8098ddada7ef1e46a911f3116d5
Jeff <jeff574@hotmail.com>
2025-09-01 06:16:52 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {} // por si alguna lib lo mira
-  },
-  build: {
-    sourcemap: true,
-    target: 'es2018'
-  }
+  define: { "process.env": {} },
+  build: { sourcemap: true, target: "es2018" },
 });
